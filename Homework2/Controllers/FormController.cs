925d76f36a8be2770ce2e32f9dda1a7a15ff407a
Michal Krzyszton <michal.krzyszton@microsoft.wsei.edu.pl>
2021-11-14 21:13:51 +0100

    Add project files.

---
+++ Homework2/Controllers/FormController.cs
@@ -0,0 +1,23 @@
+﻿using Homework2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework2.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Input(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Output", model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Homework2/Models/FormModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework2.Models
+{
+    public class FormModel
+    {
+        public int? _counter;
+
+        [Display(Name = "Imię")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Ilość powtórzeń")]
+        public int? Counter 
+        { 
+            get => _counter;
+            set
+            {
+                if (value > 5)
+                    throw new System.ArgumentOutOfRangeException();
+                else
+                    _counter = value;
+            }
+        }
+    }
+}
